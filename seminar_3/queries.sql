e5247f87d207c059ab896c4e220c4c503f51dc45
Eliasamani <eliasamanii@gmail.com>
2023-11-28 12:27:05 +0100

    Query 2 finished

--- seminar_3/queries.sql
+++ seminar_3/queries.sql
@@ -8,3 +8,25 @@ SELECT
 FROM lesson
 GROUP BY EXTRACT(YEAR FROM date), TO_CHAR(date, 'Mon')
 ORDER BY EXTRACT(YEAR FROM date), MIN(date);
+
+
+
+
+
+
+SELECT 
+    No_of_Siblings, 
+    COUNT(*) as Count
+FROM (
+    SELECT 
+        s.id, 
+        COUNT(sr.student_id) as No_of_Siblings
+    FROM 
+        student s
+        LEFT JOIN sibling_relations sr ON s.id = sr.student_id
+    GROUP BY 
+        s.id
+) as StudentSiblings
+GROUP BY 
+    No_of_Siblings
+ORDER BY No_of_Siblings ASC;
