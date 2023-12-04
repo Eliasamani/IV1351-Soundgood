8ee14734214c0ca3cb902e7e15794109c205d526
Dan Ljungström <95616308+danljungstrom@users.noreply.github.com>
2023-12-04 11:44:11 +0100

    Cleaner headers

--- seminar_3/queries.sql
+++ seminar_3/queries.sql
@@ -3,12 +3,12 @@ DEALLOCATE lessons_per_year;
 
 PREPARE lessons_per_year AS
 SELECT
-  EXTRACT(YEAR FROM date) AS Year,
-  TO_CHAR(date, 'Mon') AS Month,
+  EXTRACT(YEAR FROM date) AS "Year",
+  TO_CHAR(date, 'Mon') AS "Month",
   COUNT(*) AS Total,
-  COUNT(*) FILTER (WHERE lesson_type = 'Individual') AS Individual,
-  COUNT(*) FILTER (WHERE lesson_type = 'Group') AS Group,
-  COUNT(*) FILTER (WHERE lesson_type = 'Ensemble') AS Ensemble
+  COUNT(*) FILTER (WHERE lesson_type = 'Individual') AS "Individual",
+  COUNT(*) FILTER (WHERE lesson_type = 'Group') AS "Group",
+  COUNT(*) FILTER (WHERE lesson_type = 'Ensemble') AS "Ensemble"
 FROM lesson
 WHERE EXTRACT(YEAR FROM date) = $1
 GROUP BY EXTRACT(YEAR FROM date), TO_CHAR(date, 'Mon')
@@ -20,8 +20,8 @@ EXECUTE lessons_per_year('2023');
 
 --Query 2
 SELECT 
-    No_of_Siblings, 
-    COUNT(*) as Count
+    No_of_Siblings as "No of Siblings", 
+    COUNT(*) as "Count"
 FROM (
     SELECT 
         s.id, 
@@ -43,7 +43,10 @@ DEALLOCATE min_lessons_by_instructor;
 
 PREPARE min_lessons_by_instructor AS
 SELECT
-  i.id, p.first_name, p.last_name, COUNT(*) as No_of_Lessons
+  i.id as "ID",
+  p.first_name as "First Name", 
+  p.last_name as "Last Name", 
+  COUNT(*) as "No of Lessons"
 FROM instructor i
 JOIN person p ON i.person_id = p.id
 JOIN lesson l ON i.id = l.instructor_id
@@ -51,14 +54,13 @@ WHERE l.date >= date_trunc('month', CURRENT_DATE) -- Start of current month
   AND l.date < (date_trunc('month', CURRENT_DATE) + interval '1 month') -- End of current month
 GROUP BY i.id, p.first_name, p.last_name
 HAVING COUNT(*) > $1
-ORDER BY No_of_Lessons DESC;
+ORDER BY "No of Lessons" DESC;
 
-EXECUTE min_lessons_by_instructor('3');
+EXECUTE min_lessons_by_instructor('5');
 
 
 
 --Query 4
-CREATE MATERIALIZED View lesson_next_week AS
 SELECT 
     TO_CHAR(date, 'Day') AS Day, 
     e.genre, 
